c3337b1abe840bf58fdb0f257f208423f0720d41
IgorMatsepura <igor.matsepura@gmail.com>
2023-04-11 14:27:19 +0300

    delete fireBase

---
+++ OpenTokSimple/OpenTokSimpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenTokSimpleApp.swift
+//  OpenTokSimple
+//
+//  Created by Igor Matsepura on 11.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct OpenTokSimpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
